32ce76a98b46de37ab33d39dbb178fbb3148c8a1
Saraansh Wadkar <saraanshwadkar@gmail.com>
2022-11-15 00:56:02 -0500

    elevator states + tolerance

--- src/main/kotlin/com/team4099/robot2022/config/constants/ElevatorConstants.kt
+++ src/main/kotlin/com/team4099/robot2022/config/constants/ElevatorConstants.kt
@@ -1,5 +1,6 @@
 package com.team4099.robot2022.config.constants
 
+import com.team4099.lib.units.base.Length
 import com.team4099.lib.units.base.inches
 import com.team4099.lib.units.base.meters
 import com.team4099.lib.units.derived.volts
@@ -14,4 +15,21 @@ object ElevatorConstants {
   val ELEVATOR_KV = 0.0.volts / 1.0.meters.perSecond // TODO tune values
   val ELEVATOR_KA = 0.0.volts / 1.0.meters.perSecond.perSecond // TODO tune values
   val ELEVATOR_KG = 0.0.volts // TODO tune values
+
+  val elevatorTolerance: Length = 1.0.inches
+
+  enum class DesiredElevatorStates(val position: Length) {
+    BOTTOM_HEIGHT(0.0.inches),
+    MID_HEIGHT(16.inches),
+    MAX_HEIGHT(32.inches),
+    DUMMY(-Double.NEGATIVE_INFINITY.inches)
+  }
+
+  enum class ActualElevatorStates(val correspondingElevatorState: DesiredElevatorStates) {
+    BOTTOM(DesiredElevatorStates.BOTTOM_HEIGHT),
+    BETWEEN_BOTTOM_AND_MID(DesiredElevatorStates.DUMMY),
+    MID_HEIGHT(DesiredElevatorStates.MID_HEIGHT),
+    BETWEEN_MID_AND_MAX(DesiredElevatorStates.DUMMY),
+    MAX_HEIGHT(DesiredElevatorStates.MAX_HEIGHT)
+  }
 }
