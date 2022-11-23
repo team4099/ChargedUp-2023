e82e8d721c551bcc678f5eaa5ea91755f55ff190
Saraansh Wadkar <saraanshwadkar@gmail.com>
2022-11-23 13:23:50 -0500

    add pounds to grams unit test

--- src/test/kotlin/com/team4099/MassTest.kt
+++ src/test/kotlin/com/team4099/MassTest.kt
@@ -5,6 +5,7 @@ import com.team4099.lib.units.base.grams
 import com.team4099.lib.units.base.inGrams
 import com.team4099.lib.units.base.inKilograms
 import com.team4099.lib.units.base.inPounds
+import com.team4099.lib.units.base.pounds
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -15,6 +16,12 @@ class MassTest {
   fun testGramsToPounds() {
     val weightGrams = 4099.grams
     assertEquals(weightGrams.inPounds, weightGrams.inGrams / GRAMS_PER_POUND, kEpsilon)
+  }
+
+  @Test
+  fun testPoundsToGrams() {
+    val weightPounds = 4099.pounds
+    assertEquals(weightPounds.inGrams, weightPounds.inPounds * GRAMS_PER_POUND, kEpsilon)
   }
 
   @Test
