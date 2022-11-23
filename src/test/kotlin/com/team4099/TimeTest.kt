8fe9e7ce9c1e1b71a90810c4102b2a088d0a9462
AlphaPranav9102 <49816864+AlphaPranav9102@users.noreply.github.com>
2022-11-23 15:36:42 -0500

    added seconds to minutes test

---
+++ src/test/kotlin/com/team4099/TimeTest.kt
@@ -0,0 +1,22 @@
+package com.team4099
+
+import com.team4099.lib.units.base.SECONDS_PER_MINUTE
+import com.team4099.lib.units.base.inMinutes
+import com.team4099.lib.units.base.inSeconds
+import com.team4099.lib.units.base.seconds
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class TimeTest {
+  private val kEpsilon = 1E-9
+  private val kDelta = 1E-3
+  private val kSigma = 1E6
+  private val kAlpha = 1E9
+  private val kBeta = 1E12
+
+  @Test
+  fun testSecondsToMinutes() {
+    val seconds = 4099.seconds
+    assertEquals(seconds.inMinutes, seconds.inSeconds / SECONDS_PER_MINUTE, kEpsilon)
+  }
+}
