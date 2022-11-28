9127c5decc50b6b0d6d165c9a835745493f0282e
Matthew Choulas <MatthewChoulas@gmail.com>
2022-11-28 00:05:55 -0500

    add unit tests for charge

---
+++ src/test/kotlin/com/team4099/units/ChargeTest.kt
@@ -0,0 +1,190 @@
+package com.team4099.units
+
+import com.team4099.lib.units.derived.coulombs
+import com.team4099.lib.units.derived.inAttocoulombs
+import com.team4099.lib.units.derived.inCenticoulombs
+import com.team4099.lib.units.derived.inCoulombs
+import com.team4099.lib.units.derived.inDecacoulombs
+import com.team4099.lib.units.derived.inDecicoulombs
+import com.team4099.lib.units.derived.inExacoulombs
+import com.team4099.lib.units.derived.inFemtocoulombs
+import com.team4099.lib.units.derived.inGigacoulombs
+import com.team4099.lib.units.derived.inHectocoulombs
+import com.team4099.lib.units.derived.inKilocoulombs
+import com.team4099.lib.units.derived.inMegacoulombs
+import com.team4099.lib.units.derived.inMicrocoulombs
+import com.team4099.lib.units.derived.inMillicoulombs
+import com.team4099.lib.units.derived.inNanocoulombs
+import com.team4099.lib.units.derived.inPetacoulombs
+import com.team4099.lib.units.derived.inPicocoulombs
+import com.team4099.lib.units.derived.inTeracoulombs
+import com.team4099.lib.units.derived.inYoctocoulombs
+import com.team4099.lib.units.derived.inYottacoulombs
+import com.team4099.lib.units.derived.inZeptocoulombs
+import com.team4099.lib.units.derived.inZetacoulombs
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+/* Unit tests class for Charge.kt */
+class ChargeTest {
+  private val kEpsilon = 1E-9
+  // Hacky way to do this. Consider removing support in the future for nano and below due to
+  // floating point imprecision
+  private val kDelta = 1E-3
+  private val kSigma = 1E6
+  private val kAlpha = 1E9
+  private val kBeta = 1E12
+
+  @Test
+  fun testCoulombsToYottacoulombs() {
+    val chargeCoulombs = 4099.coulombs
+    Assertions.assertEquals(chargeCoulombs.inYottacoulombs, 4.099E-21, kEpsilon)
+  }
+
+  @Test
+  fun testCoulombsToZetacoulombs() {
+    val chargeCoulombs = 4099.coulombs
+    Assertions.assertEquals(chargeCoulombs.inZetacoulombs, 4.099E-18, kEpsilon)
+  }
+
+
+  @Test
+  fun testCoulombsToExacoulombs() {
+    val chargeCoulombs = 4099.coulombs
+    Assertions.assertEquals(chargeCoulombs.inExacoulombs, 4.099E-15, kEpsilon)
+  }
+
+  @Test
+  fun testCoulombsToPetacoulombs() {
+    val chargeCoulombs = 4099.coulombs
+    Assertions.assertEquals(chargeCoulombs.inPetacoulombs, 4.099E-12, kEpsilon)
+  }
+
+  @Test
+  fun testCoulombsToTeracoulombs() {
+    val chargeCoulombs = 4099.coulombs
+    Assertions.assertEquals(chargeCoulombs.inTeracoulombs, 4.099E-9, kEpsilon)
+  }
+
+  @Test
+  fun testCoulombsToGigacoulombs() {
+    val chargeCoulombs = 4099.coulombs
+    Assertions.assertEquals(chargeCoulombs.inGigacoulombs, 4.099E-6, kEpsilon)
+  }
+
+  @Test
+  fun testCoulombsToMegacoulombs() {
+    val chargeCoulombs = 4099.coulombs
+    Assertions.assertEquals(chargeCoulombs.inMegacoulombs, 4.099E-3, kEpsilon)
+  }
+
+  @Test
+  fun testCoulombsToKilocoulombs() {
+    val chargeCoulombs = 4099.coulombs
+    Assertions.assertEquals(chargeCoulombs.inKilocoulombs, 4.099E0, kEpsilon)
+  }
+
+  @Test
+  fun testCoulombsToHectocoulombs() {
+    val chargeCoulombs = 4099.coulombs
+    Assertions.assertEquals(chargeCoulombs.inHectocoulombs, 4.099E1, kEpsilon)
+  }
+
+  @Test
+  fun testCoulombsToDecacoulombs() {
+    val chargeCoulombs = 4099.coulombs
+    Assertions.assertEquals(chargeCoulombs.inDecacoulombs, 4.099E2, kEpsilon)
+  }
+
+  @Test
+  fun testCoulombsToDecicoulombs() {
+    val chargeCoulombs = 4099.coulombs
+    Assertions.assertEquals(chargeCoulombs.inDecicoulombs, 4.099E4, kEpsilon)
+  }
+
+  @Test
+  fun testCoulombstoCenticoulombs() {
+    val chargeCoulombs = 4099.coulombs
+    Assertions.assertEquals(chargeCoulombs.inCenticoulombs, 4.099E5, kEpsilon)
+  }
+
+  @Test
+  fun testCoulombstoMillicoulombs() {
+    val chargeCoulombs = 4099.coulombs
+    Assertions.assertEquals(chargeCoulombs.inMillicoulombs, 4.099E6, kEpsilon)
+  }
+
+  @Test
+  fun testCoulombstoMicrocoulombs() {
+    val chargeCoulombs = 4099.coulombs
+    Assertions.assertEquals(chargeCoulombs.inMicrocoulombs, 4.099E9, kEpsilon)
+  }
+
+  // Unstable due to floating point errors
+  @Test
+  fun testCoulombstoNanocoulombs() {
+    val chargeCoulombs = 4099.coulombs
+    Assertions.assertEquals(chargeCoulombs.inNanocoulombs, 4.099E12, kDelta)
+  }
+
+  // Unstable due to floating point errors
+  @Test
+  fun testCoulombstoPicocoulombs() {
+    val chargeCoulombs = 4099.coulombs
+    Assertions.assertEquals(chargeCoulombs.inPicocoulombs, 4.099E15, kSigma)
+  }
+
+  // Unstable due to floating point errors
+  @Test
+  fun testCoulombstoFemtocoulombs() {
+    val chargeCoulombs = 4099.coulombs
+    Assertions.assertEquals(chargeCoulombs.inFemtocoulombs, 4.099E18, kSigma)
+  }
+
+  // Unstable due to floating point errors
+  @Test
+  fun testCoulombstoAttocoulombs() {
+    val chargeCoulombs = 4099.coulombs
+    Assertions.assertEquals(chargeCoulombs.inAttocoulombs, 4.099E21, kSigma)
+  }
+
+  // Unstable due to floating point errors
+  @Test
+  fun testCoulombstoZeptocoulombs() {
+    val chargeCoulombs = 4099.coulombs
+    Assertions.assertEquals(chargeCoulombs.inZeptocoulombs, 4.099E24, kAlpha)
+  }
+
+  // Unstable due to floating point errors
+  @Test
+  fun testCoulombstoYoctocoulombs() {
+    val chargeCoulombs = 4099.coulombs
+    Assertions.assertEquals(chargeCoulombs.inYoctocoulombs, 4.099E27, kBeta)
+  }
+
+  @Test
+  fun testAddingCoulombs() {
+    val g1 = 1.0.coulombs
+    val g2 = 2.0.coulombs
+    Assertions.assertEquals((g1 + g2).inCoulombs, 3.0, kEpsilon)
+  }
+
+  @Test
+  fun testSubtractingCoulombs() {
+    val g1 = 1.0.coulombs
+    val g2 = 2.0.coulombs
+    Assertions.assertEquals((g2 - g1).inCoulombs, 1.0, kEpsilon)
+  }
+
+  @Test
+  fun testMultiplyingCoulombsByScalar() {
+    val g1 = 1.0.coulombs
+    Assertions.assertEquals((g1 * 4099).inCoulombs, 4099.0, kEpsilon)
+  }
+
+  @Test
+  fun testDividingCoulombsByScalar() {
+    val g1 = 4099.0.coulombs
+    Assertions.assertEquals((g1 / 4099.0).inCoulombs, 1.0, kEpsilon)
+  }
+}
---
+++ src/test/kotlin/com/team4099/units/LengthTest.kt
@@ -0,0 +1,191 @@
+package com.team4099.units
+
+
+import com.team4099.lib.units.base.inAttometers
+import com.team4099.lib.units.base.inCentimeters
+import com.team4099.lib.units.base.inDecameters
+import com.team4099.lib.units.base.inDecimeters
+import com.team4099.lib.units.base.inExameters
+import com.team4099.lib.units.base.inFemtometers
+import com.team4099.lib.units.base.inGigameters
+import com.team4099.lib.units.base.inHectometers
+import com.team4099.lib.units.base.inKilometers
+import com.team4099.lib.units.base.inMegameters
+import com.team4099.lib.units.base.inMeters
+import com.team4099.lib.units.base.inMicrometers
+import com.team4099.lib.units.base.inMillimeters
+import com.team4099.lib.units.base.inNanometers
+import com.team4099.lib.units.base.inPetameters
+import com.team4099.lib.units.base.inPicometers
+import com.team4099.lib.units.base.inTerameters
+import com.team4099.lib.units.base.inYoctometers
+import com.team4099.lib.units.base.inYottameters
+import com.team4099.lib.units.base.inZeptometers
+import com.team4099.lib.units.base.inZetameters
+import com.team4099.lib.units.base.meters
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+
+/* Unit tests class for Length.kt */
+class LengthTest {
+  private val kEpsilon = 1E-9
+  // Hacky way to do this. Consider removing support in the future for nano and below due to
+  // floating point imprecision
+  private val kDelta = 1E-3
+  private val kSigma = 1E6
+  private val kAlpha = 1E9
+  private val kBeta = 1E12
+
+  @Test
+  fun testMetersToYottameters() {
+    val lengthMeters = 4099.meters
+    assertEquals(lengthMeters.inYottameters, 4.099E-21, kEpsilon)
+  }
+
+  @Test
+  fun testMetersToZetameters() {
+    val lengthMeters = 4099.meters
+    assertEquals(lengthMeters.inZetameters, 4.099E-18, kEpsilon)
+  }
+
+
+  @Test
+  fun testMetersToExameters() {
+    val lengthMeters = 4099.meters
+    assertEquals(lengthMeters.inExameters, 4.099E-15, kEpsilon)
+  }
+
+  @Test
+  fun testMetersToPetameters() {
+    val lengthMeters = 4099.meters
+    assertEquals(lengthMeters.inPetameters, 4.099E-12, kEpsilon)
+  }
+
+  @Test
+  fun testMetersToTerameters() {
+    val lengthMeters = 4099.meters
+    assertEquals(lengthMeters.inTerameters, 4.099E-9, kEpsilon)
+  }
+
+  @Test
+  fun testMetersToGigameters() {
+    val lengthMeters = 4099.meters
+    assertEquals(lengthMeters.inGigameters, 4.099E-6, kEpsilon)
+  }
+
+  @Test
+  fun testMetersToMegameters() {
+    val lengthMeters = 4099.meters
+    assertEquals(lengthMeters.inMegameters, 4.099E-3, kEpsilon)
+  }
+
+  @Test
+  fun testMetersToKilometers() {
+    val lengthMeters = 4099.meters
+    assertEquals(lengthMeters.inKilometers, 4.099E0, kEpsilon)
+  }
+
+  @Test
+  fun testMetersToHectometers() {
+    val lengthMeters = 4099.meters
+    assertEquals(lengthMeters.inHectometers, 4.099E1, kEpsilon)
+  }
+
+  @Test
+  fun testMetersToDecameters() {
+    val lengthMeters = 4099.meters
+    assertEquals(lengthMeters.inDecameters, 4.099E2, kEpsilon)
+  }
+
+  @Test
+  fun testMetersToDecimeters() {
+    val lengthMeters = 4099.meters
+    assertEquals(lengthMeters.inDecimeters, 4.099E4, kEpsilon)
+  }
+
+  @Test
+  fun testMeterstoCentimeters() {
+    val lengthMeters = 4099.meters
+    assertEquals(lengthMeters.inCentimeters, 4.099E5, kEpsilon)
+  }
+
+  @Test
+  fun testMeterstoMillimeters() {
+    val lengthMeters = 4099.meters
+    assertEquals(lengthMeters.inMillimeters, 4.099E6, kEpsilon)
+  }
+
+  @Test
+  fun testMeterstoMicrometers() {
+    val lengthMeters = 4099.meters
+    assertEquals(lengthMeters.inMicrometers, 4.099E9, kEpsilon)
+  }
+
+  // Unstable due to floating point errors
+  @Test
+  fun testMeterstoNanometers() {
+    val lengthMeters = 4099.meters
+    assertEquals(lengthMeters.inNanometers, 4.099E12, kDelta)
+  }
+
+  // Unstable due to floating point errors
+  @Test
+  fun testMeterstoPicometers() {
+    val lengthMeters = 4099.meters
+    assertEquals(lengthMeters.inPicometers, 4.099E15, kSigma)
+  }
+
+  // Unstable due to floating point errors
+  @Test
+  fun testMeterstoFemtometers() {
+    val lengthMeters = 4099.meters
+    assertEquals(lengthMeters.inFemtometers, 4.099E18, kSigma)
+  }
+
+  // Unstable due to floating point errors
+  @Test
+  fun testMeterstoAttometers() {
+    val lengthMeters = 4099.meters
+    assertEquals(lengthMeters.inAttometers, 4.099E21, kSigma)
+  }
+
+  // Unstable due to floating point errors
+  @Test
+  fun testMeterstoZeptometers() {
+    val lengthMeters = 4099.meters
+    assertEquals(lengthMeters.inZeptometers, 4.099E24, kAlpha)
+  }
+
+  // Unstable due to floating point errors
+  @Test
+  fun testMeterstoYoctometers() {
+    val lengthMeters = 4099.meters
+    assertEquals(lengthMeters.inYoctometers, 4.099E27, kBeta)
+  }
+
+  @Test
+  fun testAddingMeters() {
+    val g1 = 1.0.meters
+    val g2 = 2.0.meters
+    assertEquals((g1 + g2).inMeters, 3.0, kEpsilon)
+  }
+
+  @Test
+  fun testSubtractingMeters() {
+    val g1 = 1.0.meters
+    val g2 = 2.0.meters
+    assertEquals((g2 - g1).inMeters, 1.0, kEpsilon)
+  }
+
+  @Test
+  fun testMultiplyingMetersByScalar() {
+    val g1 = 1.0.meters
+    assertEquals((g1 * 4099).inMeters, 4099.0, kEpsilon)
+  }
+
+  @Test
+  fun testDividingMetersByScalar() {
+    val g1 = 4099.0.meters
+    assertEquals((g1 / 4099.0).inMeters, 1.0, kEpsilon)
+  }
+}
