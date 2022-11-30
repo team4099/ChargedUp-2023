34fe92102164efdd8260275fe1ca4b36a70fb891
Saraansh Wadkar <saraanshwadkar@gmail.com>
2022-11-30 15:38:53 -0500

    initial telescoping climber files

---
+++ src/main/kotlin/com/team4099/robot2022/subsystems/telescopingclimber/TelescopingClimberIO.kt
@@ -0,0 +1,17 @@
+package com.team4099.robot2022.subsystems.climber
+
+import org.littletonrobotics.junction.LogTable
+import org.littletonrobotics.junction.inputs.LoggableInputs
+
+interface TelescopingClimberIO {
+  class TelescopingClimberIOInputs : LoggableInputs {
+
+    override fun toLog(table: LogTable?) {
+      TODO("Not yet implemented")
+    }
+
+    override fun fromLog(table: LogTable?) {
+      TODO("Not yet implemented")
+    }
+  }
+}
