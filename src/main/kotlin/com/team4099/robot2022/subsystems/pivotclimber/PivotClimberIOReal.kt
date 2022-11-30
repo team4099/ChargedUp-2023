572830c23e15e555933d5a1a4e3c8a09e86986e2
Saraansh Wadkar <saraanshwadkar@gmail.com>
2022-11-30 15:37:50 -0500

    initial pivot climber files

---
+++ src/main/kotlin/com/team4099/robot2022/subsystems/pivotclimber/PivotClimberIOReal.kt
@@ -0,0 +1,7 @@
+package com.team4099.robot2022.subsystems.climber
+
+object PivotClimberIOReal : PivotClimberIO {
+  override fun updateInputs(io: PivotClimberIO.PivotClimberIOInputs) {
+    TODO("Not yet implemented")
+  }
+}
