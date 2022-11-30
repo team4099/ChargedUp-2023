0e505ec466e0c81aae1bb19f135c94b94284c906
Saraansh Wadkar <saraanshwadkar@gmail.com>
2022-11-30 15:38:53 -0500

    initial telescoping climber files

---
+++ src/main/kotlin/com/team4099/robot2022/subsystems/telescopingclimber/TelescopingClimber.kt
@@ -0,0 +1,5 @@
+package com.team4099.robot2022.subsystems.climber
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+
+class TelescopingClimber(val io: TelescopingClimberIO) : SubsystemBase()
---
+++ src/main/kotlin/com/team4099/robot2022/subsystems/telescopingclimber/TelescopingClimberIOReal.kt
@@ -0,0 +1,3 @@
+package com.team4099.robot2022.subsystems.climber
+
+object TelescopingClimberIOReal : TelescopingClimberIO
