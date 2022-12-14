bb3de3a36b16e779797cd7f8656a0c99a29f13d5
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-12-13 21:51:59 -0500

    Temperature unit class

---
+++ src/test/kotlin/com/team4099/units/TemperatureTest.kt
@@ -0,0 +1,38 @@
+package com.team4099.units
+
+import com.team4099.lib.units.base.celsius
+import com.team4099.lib.units.base.fahrenheit
+import com.team4099.lib.units.base.inCelsius
+import com.team4099.lib.units.base.inFahrenheit
+import com.team4099.lib.units.base.kelvin
+import com.team4099.lib.units.base.inKelvins
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class TemperatureTest {
+  private val kEpsilon = 1E-9
+
+  @Test
+  fun testCelsiusToKelvins() {
+    val temperature = 0.0.celsius
+    assertEquals(temperature.inKelvins, 273.15, kEpsilon)
+  }
+
+  @Test
+  fun testFahrenheitToKelvins() {
+    val temperature = 32.0.fahrenheit
+    assertEquals(temperature.inKelvins, 273.15, kEpsilon)
+  }
+
+  @Test
+  fun testKelvinsToCelsius() {
+    val temperature = 273.15.kelvin
+    assertEquals(temperature.inCelsius, 0.0, kEpsilon)
+  }
+
+  @Test
+  fun testKelvinsToFahrenheit() {
+    val temperature = 273.15.kelvin
+    assertEquals(temperature.inFahrenheit, 32.0, kEpsilon)
+  }
+}
