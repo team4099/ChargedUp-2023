1250cb03faaa294fba811fbbeef98a5dc7b6ad28
Eckart Schneider <eckshn@gmail.com>
2022-12-14 13:17:43 -0500

    Create Twist2d unit tests

---
+++ src/test/kotlin/com/team4099/geometry/Twist2dTest.kt
@@ -0,0 +1,77 @@
+package com.team4099.geometry
+
+import com.team4099.lib.geometry.Pose2d
+import com.team4099.lib.geometry.Rotation2d
+import com.team4099.lib.geometry.Twist2d
+import com.team4099.lib.units.base.meters
+import com.team4099.lib.units.derived.degrees
+import com.team4099.lib.units.derived.radians
+import com.team4099.lib.units.perSecond
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotEquals
+import org.junit.jupiter.api.Test
+
+class Twist2dTest {
+
+  // need to individually test each variable
+  @Test
+  fun testStraight() {
+    val straight = Twist2d(5.0.meters.perSecond, 0.0.meters.perSecond, 0.0.radians.perSecond)
+    val straightPose = Pose2d().exp(straight)
+    val expected = Pose2d(5.0.meters, 0.0.meters, Rotation2d())
+    assertEquals(expected, straightPose)
+  }
+
+  @Test
+  fun testQuarterCircle() {
+    val quarterCircle =
+      Twist2d(
+        (5.0 / 2.0 * Math.PI).meters.perSecond,
+        0.0.meters.perSecond,
+        (Math.PI / 2.0).radians.perSecond
+      )
+    val quarterCirclePose = Pose2d().exp(quarterCircle)
+    val expected = Pose2d(5.0.meters, 5.0.meters, Rotation2d(90.0.degrees))
+    assertEquals(expected, quarterCirclePose)
+  }
+
+  @Test
+  fun testDiagonalNoDtheta() {
+    val diagonal = Twist2d(2.0.meters.perSecond, 2.0.meters.perSecond, 0.0.radians.perSecond)
+    val diagonalPose = Pose2d().exp(diagonal)
+    val expected = Pose2d(2.0.meters, 2.0.meters, Rotation2d())
+    assertEquals(expected, diagonalPose)
+  }
+
+  @Test
+  fun testEquality() {
+    val one = Twist2d(5.meters.perSecond, 1.meters.perSecond, 3.radians.perSecond)
+    val two = Twist2d(5.meters.perSecond, 1.meters.perSecond, 3.radians.perSecond)
+    assertEquals(one, two)
+  }
+
+  @Test
+  fun testInequality() {
+    val one = Twist2d(5.meters.perSecond, 1.0.meters.perSecond, 3.radians.perSecond)
+    val two = Twist2d(5.meters.perSecond, 1.2.meters.perSecond, 3.radians.perSecond)
+    assertNotEquals(one, two)
+  }
+
+  @Test
+  fun testPose2dLog() {
+    val start = Pose2d()
+    val end = Pose2d(5.0.meters, 5.0.meters, Rotation2d(90.0.degrees))
+    val twist = start.log(end)
+    val expected =
+      Twist2d(
+        (5.0 / 2.0 * Math.PI).meters.perSecond,
+        0.0.meters.perSecond,
+        (Math.PI / 2.0).radians.perSecond
+      )
+    assertEquals(expected, twist)
+
+    // Make sure computed twist gives back original end pose
+    val reapplied = start.exp(twist)
+    assertEquals(end, reapplied)
+  }
+}
