831d9e0efbb087b48c16f55b9304689dc883c2a4
Saraansh Wadkar <saraanshwadkar@gmail.com>
2022-12-25 17:01:34 -0500

    add charged up logo to repo

--- src/main/kotlin/com/team4099/robot2023/BuildConstants.kt
+++ src/main/kotlin/com/team4099/robot2023/BuildConstants.kt
@@ -4,12 +4,12 @@ package com.team4099.robot2023
  * Automatically generated file containing build version information.
  */
 const val MAVEN_GROUP = ""
-const val MAVEN_NAME = "OffseasonDevelopment"
+const val MAVEN_NAME = "ChargedUp-2023"
 const val VERSION = "unspecified"
-const val GIT_REVISION = 59
-const val GIT_SHA = "f5ab03283d68a0d05ad0de17bdf111852fac982f"
-const val GIT_DATE = "2022-12-24T23:45:48Z"
-const val GIT_BRANCH = "swervedev"
-const val BUILD_DATE = "2022-12-25T11:52:29Z"
-const val BUILD_UNIX_TIME = 1671987149370L
+const val GIT_REVISION = 64
+const val GIT_SHA = "b54490e6862f8bf0cbca57d8132564ef91a13749"
+const val GIT_DATE = "2022-12-25T16:57:02Z"
+const val GIT_BRANCH = "main"
+const val BUILD_DATE = "2022-12-25T16:58:38Z"
+const val BUILD_UNIX_TIME = 1672005518137L
 const val DIRTY = 0
