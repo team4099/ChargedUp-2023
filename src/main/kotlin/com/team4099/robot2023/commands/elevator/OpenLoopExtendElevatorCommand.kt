1d66d73e2274554ab9bd94d6e7e61e54276965cc
CodingMaster121 <ytryanlo@gmail.com>
2023-01-09 18:46:26 -0500

    Initial Elevator

--- src/main/kotlin/com/team4099/robot2023/commands/elevator/OpenLoopExtendElevatorCommand.kt
+++ src/main/kotlin/com/team4099/robot2023/commands/elevator/OpenLoopExtendElevatorCommand.kt
@@ -1,23 +1,20 @@
 package com.team4099.robot2023.commands.elevator
 
-import com.team4099.robot2023.config.constants.ElevatorConstants
 import com.team4099.robot2023.subsystems.elevator.Elevator
 import edu.wpi.first.wpilibj2.command.CommandBase
 import org.littletonrobotics.junction.Logger
+import org.team4099.lib.units.derived.volts
 
 class OpenLoopExtendElevatorCommand(val elevator: Elevator) : CommandBase() {
   init {
     addRequirements(elevator)
   }
 
-  override fun initialize() {
-    elevator.desiredState = ElevatorConstants.DesiredElevatorStates.MAX_HEIGHT
-  }
-
   override fun execute() {
-    elevator.setOpenLoop(1.0)
+    elevator.setOutputVoltage(12.volts)
     Logger.getInstance().recordOutput("ActiveCommands/OpenLoopExtendElevatorCommand", true)
   }
+
   override fun isFinished(): Boolean {
     return false
   }
