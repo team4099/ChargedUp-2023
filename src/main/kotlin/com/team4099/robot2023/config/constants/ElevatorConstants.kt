bba3fa64b12ac5c73cba73f49f531a2fc1114671
AgentNJW33 <nikhil.j.whig@gmail.com>
2023-01-09 19:33:28 -0500

    Finish elevator constants

--- src/main/kotlin/com/team4099/robot2023/config/constants/ElevatorConstants.kt
+++ src/main/kotlin/com/team4099/robot2023/config/constants/ElevatorConstants.kt
@@ -12,11 +12,11 @@ object ElevatorConstants {
     // TODO(Check height with design)
     GROUND_INTAKE_HEIGHT(1.inches),
     // TODO(Check height with design)
-    SHELF_INTAKE_HEIGHT(15.inches),
-    // TODO(Check height with design)
     LOW_SCORE_HEIGHT(1.5.inches),
     // TODO(Check height with design)
-    MID_SCORE_HEIGHT(20.inches),
+    MID_SCORE_HEIGHT(10.inches),
+    // TODO(Check height with design)
+    SHELF_INTAKE_HEIGHT(15.inches),
     // TODO(Check height with design)
     HIGH_SCORE_HEIGHT(30.inches),
     // TODO(Check height with design)
@@ -25,8 +25,18 @@ object ElevatorConstants {
   }
 
   enum class ActualElevatorStates(val correspondingDesiredState: DesiredElevatorStates) {
-    START(DesiredElevatorStates.MIN_HEIGHT),
+    MIN_HEIGHT(DesiredElevatorStates.MIN_HEIGHT),
     BETWEEN_MIN_AND_GROUND_INTAKE(DesiredElevatorStates.DUMMY),
-    GROUND_INTAKE(DesiredElevatorStates.GROUND_INTAKE_HEIGHT)
+    GROUND_INTAKE(DesiredElevatorStates.GROUND_INTAKE_HEIGHT),
+    BETWEEN_GROUND_INTAKE_AND_LOW(DesiredElevatorStates.DUMMY),
+    LOW_SCORE_HEIGHT(DesiredElevatorStates.LOW_SCORE_HEIGHT),
+    BETWEEN_LOW_AND_MID(DesiredElevatorStates.DUMMY),
+    MID_SCORE_HEIGHT(DesiredElevatorStates.MID_SCORE_HEIGHT),
+    BETWEEN_MID_AND_SHELF_INTAKE(DesiredElevatorStates.DUMMY),
+    SHELF_INTAKE_HEIGHT(DesiredElevatorStates.SHELF_INTAKE_HEIGHT),
+    BETWEEN_SHELF_INTAKE_AND_HIGH(DesiredElevatorStates.DUMMY),
+    HIGH_SCORE_HEIGHT(DesiredElevatorStates.HIGH_SCORE_HEIGHT),
+    BETWEEN_HIGH_AND_MAX(DesiredElevatorStates.DUMMY),
+    MAX_HEIGHT(DesiredElevatorStates.MAX_HEIGHT)
   }
 }
