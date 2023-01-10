caec8dfa0ca4ad08ce4c905a4fd60db1e54627b4
Saraansh Wadkar <saraanshwadkar@gmail.com>
2023-01-09 19:28:21 -0500

    add apriltag classes w units

--- src/main/kotlin/com/team4099/robot2023/subsystems/vision/Vision.kt
+++ src/main/kotlin/com/team4099/robot2023/subsystems/vision/Vision.kt
@@ -1,5 +1,12 @@
 package com.team4099.robot2023.subsystems.vision
 
+import com.team4099.apriltag.AprilTagFieldLayout
+import com.team4099.robot2023.config.constants.FieldConstants
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 
-class Vision(val io: VisionIO) : SubsystemBase()
+class Vision(val io: VisionIO) : SubsystemBase() {
+  val layout: AprilTagFieldLayout =
+    AprilTagFieldLayout(
+      FieldConstants.aprilTags, FieldConstants.fieldLength, FieldConstants.fieldWidth
+    )
+}
