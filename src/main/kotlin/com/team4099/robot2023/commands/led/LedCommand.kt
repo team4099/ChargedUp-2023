56dd36fa9d51cc7bad5d450efda9c619f50ca25e
Matthew Choulas <MatthewChoulas@gmail.com>
2023-01-10 19:40:07 -0500

    finish blinkin states

--- src/main/kotlin/com/team4099/robot2023/commands/led/LedCommand.kt
+++ src/main/kotlin/com/team4099/robot2023/commands/led/LedCommand.kt
@@ -1,10 +1,13 @@
 package com.team4099.robot2023.commands.led
 
+<<<<<<< HEAD
 import com.ctre.phoenix.led.CANdle.LEDStripType
 import com.team4099.robot2023.Robot
 import com.team4099.robot2023.commands.drivetrain.TeleopDriveCommand
 import com.team4099.robot2023.config.constants.LedConstants
 import com.team4099.robot2023.subsystems.drivetrain.drive.Drivetrain
+=======
+>>>>>>> 3e56206 (finish blinkin states)
 import com.team4099.robot2023.subsystems.led.Led
 import edu.wpi.first.wpilibj2.command.CommandBase
 import org.team4099.lib.units.derived.inDegrees
@@ -13,7 +16,11 @@ import java.lang.Math.abs
 class LedCommand(val led: Led, val drivetrain: Drivetrain) : CommandBase() {
 
   init {
+<<<<<<< HEAD
     addRequirements(led, drivetrain)
+=======
+    addRequirements(led)
+>>>>>>> 3e56206 (finish blinkin states)
   }
 
   override fun execute() {
@@ -22,6 +29,7 @@ class LedCommand(val led: Led, val drivetrain: Drivetrain) : CommandBase() {
           LedConstants.LEDMode.DISABLED
         }
     */
+<<<<<<< HEAD
 
     if (kotlin.math.abs(drivetrain.gyroInputs.gyroPitch.inDegrees) > 2.5) {
       if (drivetrain.gyroInputs.gyroPitch.inDegrees > 2.5) {
@@ -42,5 +50,7 @@ class LedCommand(val led: Led, val drivetrain: Drivetrain) : CommandBase() {
         Robot.isDisabled -> LedConstants.LEDMode.DISABLED
       }
     }
+=======
+>>>>>>> 3e56206 (finish blinkin states)
   }
 }
