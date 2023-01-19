1befc8c92c29620987ab06b52ef589047f1d1e56
Saraansh Wadkar <saraanshwadkar@gmail.com>
2023-01-18 22:28:35 -0500

    lower drift constant

--- src/main/kotlin/com/team4099/robot2023/config/constants/Constants.kt
+++ src/main/kotlin/com/team4099/robot2023/config/constants/Constants.kt
@@ -23,7 +23,7 @@ object Constants {
 
     val TUNING_MODE = true
     val SIMULATE_DRIFT = true
-    val DRIFT_CONSTANT = 0.005
+    val DRIFT_CONSTANT = 0.001
 
     enum class RobotType {
       REAL,
