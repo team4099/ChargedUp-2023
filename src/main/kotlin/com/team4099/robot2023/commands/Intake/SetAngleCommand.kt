97ea7fd126faaa87e482486446602525d56708bb
Pratheek Ramakrishna <pramakrishna@team4099.com>
2023-01-19 19:10:51 -0500

    Started setAngleCommand and changed Neo_kt to Arm_kt

--- src/main/kotlin/com/team4099/robot2023/commands/Intake/SetAngleCommand.kt
+++ src/main/kotlin/com/team4099/robot2023/commands/Intake/SetAngleCommand.kt
@@ -2,8 +2,6 @@ package com.team4099.robot2023.commands.Intake
  import com.ctre.phoenix.motorcontrol.ControlMode
 import com.ctre.phoenix.motorcontrol.DemandType
 import com.ctre.phoenix.motorcontrol.NeutralMode
-import com.ctre.phoenix.motorcontrol.can.TalonFX
-import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration
 import com.team4099.lib.units.derived.ElectricalPotential
 import com.team4099.lib.units.derived.inVolts
 import com.team4099.lib.units.derived.volts
