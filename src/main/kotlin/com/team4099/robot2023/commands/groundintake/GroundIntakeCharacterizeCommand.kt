2c6aec35e528f9ae202554dffd1f1e989f1fba6f
Matthew Choulas <MatthewChoulas@gmail.com>
2023-01-31 18:21:13 -0500

    log characterization using advantage scope

--- src/main/kotlin/com/team4099/robot2023/commands/groundintake/GroundIntakeCharacterizeCommand.kt
+++ src/main/kotlin/com/team4099/robot2023/commands/groundintake/GroundIntakeCharacterizeCommand.kt
@@ -2,6 +2,7 @@ package com.team4099.robot2023.commands.elevator
 
 import com.team4099.robot2023.subsystems.groundintake.GroundIntake
 import edu.wpi.first.wpilibj2.command.CommandBase
+import org.littletonrobotics.junction.Logger
 import org.team4099.lib.units.derived.degrees
 import org.team4099.lib.units.derived.inVolts
 import org.team4099.lib.units.derived.volts
@@ -37,6 +38,7 @@ class GroundIntakeCharacterizeCommand(val groundIntake: GroundIntake) : CommandB
     if (groundIntake.inputs.armVelocity > 0.0.degrees.perSecond) {
       hasMoved = true
       println(appliedVolts.inVolts)
+      Logger.getInstance().recordOutput("GroundIntake/CharacterizationOutput", appliedVolts.inVolts)
     }
 
     appliedVolts += step
