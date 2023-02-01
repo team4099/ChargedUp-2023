936e895edae302d090a34a0785128f5a2106c166
Matthew Choulas <MatthewChoulas@gmail.com>
2023-02-01 18:25:37 -0500

    add tolerance for manipulator arm characterization

--- src/main/kotlin/com/team4099/robot2023/commands/manipulator/ArmCharacterizationCommand.kt
+++ src/main/kotlin/com/team4099/robot2023/commands/manipulator/ArmCharacterizationCommand.kt
@@ -1,9 +1,10 @@
 package com.team4099.robot2023.commands.manipulator
 
 import com.team4099.robot2023.subsystems.manipulator.Manipulator
+import edu.wpi.first.wpilibj.RobotBase
 import edu.wpi.first.wpilibj2.command.CommandBase
 import org.littletonrobotics.junction.Logger
-import org.team4099.lib.units.base.meters
+import org.team4099.lib.units.base.inches
 import org.team4099.lib.units.derived.inVolts
 import org.team4099.lib.units.derived.volts
 import org.team4099.lib.units.perSecond
@@ -23,17 +24,31 @@ class ArmCharacterizationCommand(val manipulator: Manipulator) : CommandBase() {
 
   var hasMoved = false
   var appliedVolts = 0.volts
-  var step = 0.001.volts
+
+  var sim_step = 0.001.volts
+  var real_step = 0.01.volts
+
+  var moveTolerance = 0.1.inches.perSecond
+
+  override fun initialize() {
+    hasMoved = false
+    appliedVolts = 0.volts
+  }
 
   override fun execute() {
     manipulator.setArmVoltage(appliedVolts)
 
-    if (manipulator.inputs.armVelocity > 0.meters.perSecond && !hasMoved) {
+    if ((manipulator.inputs.armVelocity - 0.0.inches.perSecond).absoluteValue > moveTolerance) {
       hasMoved = true
       println(appliedVolts.inVolts)
       Logger.getInstance().recordOutput("/Manipulator/ksVoltage", appliedVolts.inVolts)
     }
-    appliedVolts += step
+
+    if (RobotBase.isReal()) {
+      appliedVolts += real_step
+    } else {
+      appliedVolts += sim_step
+    }
   }
 
   override fun isFinished(): Boolean {
