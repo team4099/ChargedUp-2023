7ac831e610ab61021d94708c2191e81698eec99c
Matthew Choulas <MatthewChoulas@gmail.com>
2023-02-01 00:09:37 -0500

    add characterization movement tolerance

--- src/main/kotlin/com/team4099/robot2023/commands/groundintake/GroundIntakeCharacterizeCommand.kt
+++ src/main/kotlin/com/team4099/robot2023/commands/groundintake/GroundIntakeCharacterizeCommand.kt
@@ -1,6 +1,7 @@
 package com.team4099.robot2023.commands.elevator
 
 import com.team4099.robot2023.subsystems.groundintake.GroundIntake
+import edu.wpi.first.wpilibj.RobotBase
 import edu.wpi.first.wpilibj2.command.CommandBase
 import org.littletonrobotics.junction.Logger
 import org.team4099.lib.units.derived.degrees
@@ -24,24 +25,31 @@ class GroundIntakeCharacterizeCommand(val groundIntake: GroundIntake) : CommandB
 
   var hasMoved = false
   var appliedVolts = 0.volts
-  var step = 0.01.volts
+
+  var sim_step = 0.01.volts
+  var real_step = 0.1.volts
+
+  var moveTolerance = 0.1.degrees.perSecond
 
   override fun initialize() {
     hasMoved = false
     appliedVolts = 0.volts
-    step = 0.01.volts
   }
 
   override fun execute() {
     groundIntake.io.setArmVoltage(appliedVolts)
 
-    if (groundIntake.inputs.armVelocity > 0.0.degrees.perSecond) {
+    if ((groundIntake.inputs.armVelocity - 0.0.degrees.perSecond).absoluteValue > moveTolerance) {
       hasMoved = true
       println(appliedVolts.inVolts)
       Logger.getInstance().recordOutput("GroundIntake/CharacterizationOutput", appliedVolts.inVolts)
     }
 
-    appliedVolts += step
+    if (RobotBase.isReal()) {
+      appliedVolts += real_step
+    } else {
+      appliedVolts += sim_step
+    }
   }
 
   override fun isFinished(): Boolean {
