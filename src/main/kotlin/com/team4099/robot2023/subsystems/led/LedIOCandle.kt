1041476c25c8fef314d487b75dea0e85ae78bfc8
AlphaPranav9102 <49816864+AlphaPranav9102@users.noreply.github.com>
2023-03-07 20:20:33 -0500

    change to canivore

--- src/main/kotlin/com/team4099/robot2023/subsystems/led/LedIOCandle.kt
+++ src/main/kotlin/com/team4099/robot2023/subsystems/led/LedIOCandle.kt
@@ -7,7 +7,7 @@ import com.team4099.robot2023.config.constants.LedConstants.LEDMode
 
 object LedIOCandle : LedIO {
 
-  private val ledController = CANdle(Constants.Led.LED_CANDLE_ID)
+  private val ledController = CANdle(Constants.Led.LED_CANDLE_ID, Constants.Universal.CANIVORE_NAME)
   private var lastState: LEDMode = LEDMode.IDLE
 
   override fun updateInputs(inputs: LedIO.LedIOInputs) {
