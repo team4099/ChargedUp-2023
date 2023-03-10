c1d8c65f772f83549767dfc01f935d4e6a9cafa2
Saraansh Wadkar <saraanshwadkar@gmail.com>
2023-03-10 10:22:41 -0500

    use 6328 constants

--- src/main/kotlin/com/team4099/robot2023/config/constants/DrivetrainConstants.kt
+++ src/main/kotlin/com/team4099/robot2023/config/constants/DrivetrainConstants.kt
@@ -117,7 +117,7 @@ object DrivetrainConstants {
 
     val AUTO_THETA_ALLOWED_ERROR = 3.degrees
 
-    val AUTO_THETA_PID_KP = 30.degrees.perSecond / 1.degrees
+    val AUTO_THETA_PID_KP = 8.degrees.perSecond / 1.degrees
     val AUTO_THETA_PID_KI = 0.0.degrees.perSecond / (1.degrees * 1.seconds)
     val AUTO_THETA_PID_KD =
       (0.0.degrees.perSecond / (1.degrees / 1.seconds)).radiansPerSecondPerRadiansPerSecond
