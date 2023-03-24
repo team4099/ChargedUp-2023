e5249120f03e55e6c40167a29a90f7bf4213e2fd
Saraansh Wadkar <saraanshwadkar@gmail.com>
2023-03-24 08:44:39 -0400

    speed up elevator

--- src/main/kotlin/com/team4099/robot2023/config/constants/ElevatorConstants.kt
+++ src/main/kotlin/com/team4099/robot2023/config/constants/ElevatorConstants.kt
@@ -69,8 +69,8 @@ object ElevatorConstants {
   // circumference / 2pi = radius
   val SPOOL_RADIUS = 0.005.meters * 32.0 / (2 * PI)
 
-  val MAX_VELOCITY = 75.inches.perSecond // 150
-  val MAX_ACCELERATION = 225.inches.perSecond.perSecond // 500
+  val MAX_VELOCITY = 150.inches.perSecond // 75
+  val MAX_ACCELERATION = 500.inches.perSecond.perSecond // 225
 
   val ELEVATOR_MAX_EXTENSION = 54.8.inches
   val ELEVATOR_MAX_RETRACTION = 0.0.inches
