a03be318fccd71a7babd6c384b7b8e23e151b801
Shom770 <82843611+Shom770@users.noreply.github.com>
2023-07-20 01:43:27 -0400

    Fix control board having the wrong controls

--- src/main/kotlin/com/team4099/robot2023/config/ControlBoard.kt
+++ src/main/kotlin/com/team4099/robot2023/config/ControlBoard.kt
@@ -62,21 +62,13 @@ object ControlBoard {
   val scoreOuttake = Trigger { driver.xButton }
   val groundIntakeCube = Trigger { driver.rightShoulderButton }
 
-<<<<<<< Updated upstream
-//  val increaseRollerVoltage = Trigger { operator.dPadUp }
-//  val decreaseRollerVoltage = Trigger { operator.dPadDown }
+  val increaseRollerVoltage = Trigger { operator.dPadUp }
+  val decreaseRollerVoltage = Trigger { operator.dPadDown }
 
   val groundIntakeCone = Trigger { driver.yButton }
-  val autoScore = Trigger { driver.dPadDown}
-//  val dpadDown = Trigger { driver.dPadDown }
-=======
-  //  val increaseRollerVoltage = Trigger { operator.dPadUp }
-  //  val decreaseRollerVoltage = Trigger { operator.dPadDown }
+  val autoScore = Trigger { driver.rightTriggerAxis > 0.5 }
+  val dpadDown = Trigger { driver.dPadDown }
 
-  val groundIntakeCone = Trigger { driver.yButton }
-  val autoScore = Trigger { driver.dPadDown }
-  //  val dpadDown = Trigger { driver.dPadDown }
->>>>>>> Stashed changes
 
   val ejectGamePiece = Trigger { operator.dPadRight }
 
