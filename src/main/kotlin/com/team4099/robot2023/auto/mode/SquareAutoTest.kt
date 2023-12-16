be4157271bbaa1bbaa993eda38ef1972761e6737
Shilab66 <89350258+Shilab66@users.noreply.github.com>
2023-12-16 16:32:04 -0500

    add forward, left, and square auto paths

---
+++ src/main/kotlin/com/team4099/robot2023/auto/mode/SquareAutoTest.kt
@@ -0,0 +1,50 @@
+package com.team4099.robot2023.auto.mode
+
+import com.team4099.lib.trajectory.Waypoint
+import com.team4099.robot2023.commands.drivetrain.DrivePathCommand
+import com.team4099.robot2023.subsystems.drivetrain.drive.Drivetrain
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
+import org.team4099.lib.geometry.Translation2d
+import org.team4099.lib.units.base.meters
+import org.team4099.lib.units.derived.degrees
+import org.team4099.lib.units.derived.inRotation2ds
+
+class SquareAutoTest(val drivetrain: Drivetrain) : SequentialCommandGroup() {
+  init {
+    val startingPosX = drivetrain.odometryPose.x
+    val startingPosY = drivetrain.odometryPose.y
+    addRequirements(drivetrain)
+
+    addCommands(
+      DrivePathCommand(
+        drivetrain,
+        {
+          listOf(
+            Waypoint(
+              Translation2d(startingPosX, startingPosY).translation2d,
+              null,
+              0.0.degrees.inRotation2ds
+            ),
+            Waypoint(
+              Translation2d(startingPosX+1.0.meters, startingPosY).translation2d,
+              null, 0.0.degrees.inRotation2ds
+            ),
+            Waypoint(
+              Translation2d(startingPosX+1.0.meters, startingPosY+1.0.meters).translation2d,
+              null, 0.0.degrees.inRotation2ds
+            ),
+            Waypoint(
+              Translation2d(startingPosX+1.meters, startingPosY).translation2d,
+              null, 0.0.degrees.inRotation2ds
+            ),
+            Waypoint(
+              Translation2d(startingPosX, startingPosY).translation2d,
+              null, 0.0.degrees.inRotation2ds
+            ),
+          )
+        },
+        resetPose = true
+      )
+    )
+  }
+}
